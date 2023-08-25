f0b78244529a098152d4ce7cb95a14cff45b7c1d
Irfan Ullah <iukhan53@gmail.com>
2023-08-26 03:35:39 +0500

    Completed Portfolio

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,11 +3,19 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta property="og:title" content="IUKHAN - Full Stack Web Developer">
+  <meta property="og:description" content="Specializing in Laravel, TALL Stack, PWA, and SAAS applications.">
+  <meta property="og:image" content="{{ asset('/img/logo.png') }}">
+  <meta property="og:url" content="https://iukhan.codes">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta property="og:site_name" content="IUKHAN">
 
-  <title>IUKHAN - Full Stack Web Developer</title>
+  <title>IUKHAN - Full Stack Web Developer | Laravel | TALL Stack | PWA | SAAS</title>
 
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta name="description" content="IUKHAN - Full Stack Web Developer">
+  <meta name="description" content="IUKHAN is a Full Stack Web Developer specializing in Laravel, TALL Stack, PWA, and SAAS applications.">
+  <link rel="canonical" href="https://iukhan.codes" />
+
   <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
   <link rel="icon" href="{{ asset('/img/logo.png') }}" type="image/png">
   <script src="https://apis.google.com/js/platform.js"></script>
